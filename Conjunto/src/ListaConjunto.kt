07d9c541e6cd8878a18b03aef7bc56e8349257dc
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2022-05-09 09:35:29 -0300

    lista conjunto

---
+++ Conjunto/src/ListaConjunto.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val numeros: MutableSet<Int> = mutableSetOf<Int>()
+numeros.add(1)
+numeros.add(5)
+numeros.add(5)
+   numeros.add(6)
+    numeros.add(7)
+    numeros.add(8)
+    numeros.add(8)
+    println(numeros)
+}
