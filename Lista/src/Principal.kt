bb753dc4394f9f6fb2921ce3226114ae89c3f6c2
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2022-05-09 09:45:48 -0300

    Lista

---
+++ Lista/src/Principal.kt
@@ -0,0 +1,15 @@
+
+
+    fun main() {
+    val numeros: MutableList<Int> = mutableListOf<Int>()
+    numeros.add(1)
+    numeros.add(5)
+    numeros.add(5)
+    numeros.add(6)
+    numeros.add(7)
+    numeros.ad8)
+   numeros.add(8)
+    println(numeros)
+
+
+}
